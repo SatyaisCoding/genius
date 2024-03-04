e55a632db7285e8bf417a36f6addb3567665c869
satya740 <satyaprakash74029@gmail.com>
2024-03-04 22:21:59 +0530

    authentication

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import React from 'react';
+import Link from 'next/link';
+
 
 function LandingPage() {
-  return (
-    <div>
-        Landing Page (Unprotected)
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                Landing Page (Unprotected)
+            </div>
+
+            <Link href="/sign-in">
+                <Button>
+                    Login
+                </Button>
+            </Link>
+            <Link href="/sign-up">
+                <Button>
+                    Register
+                </Button>
+            </Link>
+        </div>
+    )
 }
 
 export default LandingPage;
